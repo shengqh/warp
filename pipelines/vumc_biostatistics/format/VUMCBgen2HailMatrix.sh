4a4ef9df5448d02946049e87df6078eea74122f1
Quanhu Sheng <shengqh@gmail.com>
2024-12-28 18:03:58 -0600

    VUMCBgen2HailMatrix

---
+++ pipelines/vumc_biostatistics/format/VUMCBgen2HailMatrix.sh
@@ -0,0 +1,21 @@
+mkdir -p /nobackup/h_cqs/shengq2/biovu/agd163k/chrom-msvcf/cromwell
+cd /nobackup/h_cqs/shengq2/biovu/agd163k/chrom-msvcf/cromwell
+
+if [[ ! -s cromwell_finalOutputs/agd163k_chrM.primary_pass.mt.tar.gz ]]; then
+  echo "Preparing agd163k_chrM.primary_pass.mt"
+  java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
+    -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+    run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/format/VUMCBgen2HailMatrix.wdl \
+    -i /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/format/VUMCBgen2HailMatrix.inputs_chrM.json \
+    --options /data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json
+fi
+
+if [[ ! -s cromwell_finalOutputs/agd163k_chr22.primary_pass.mt.tar.gz ]]; then
+  echo "Preparing agd163k_chr22.primary_pass.mt"
+  java -Dconfig.file=/data/cqs/softwares/cqsperl/config/wdl/cromwell.local.conf \
+    -jar /data/cqs/softwares/wdl/cromwell-84.jar \
+    run /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/format/VUMCBgen2HailMatrix.wdl \
+    -i /nobackup/h_cqs/shengq2/program/warp/pipelines/vumc_biostatistics/format/VUMCBgen2HailMatrix.inputs_chr22.json \
+    --options /data/cqs/softwares/cqsperl/config/wdl/cromwell.options.json
+fi
+
