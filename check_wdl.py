d1e98314535b68257c96b3c612aba8dfdaa8e3ed
Quanhu Sheng <shengqh@gmail.com>
2026-05-11 15:55:01 -0500

    cromwell 90

--- check_wdl.py
+++ check_wdl.py
@@ -5,4 +5,4 @@ with open('/nobackup/h_cqs/shengq2/program/warp/.dockstore.yml', "rt") as fin:
     if "primaryDescriptorPath:" in line:
       filepath = '/nobackup/h_cqs/shengq2/program/warp' + line.split(":")[1].strip()
       print(filepath)
-      subprocess.run(['java', '-jar', '/data/cqs/softwares/wdl/womtool-84.jar', 'validate', filepath])
+      subprocess.run(['java', '-jar', '/data/cqs/softwares/cromwell/womtool-90.jar', 'validate', filepath])
